aa27c1e70b54faa551073d39342ba997219b1480
Thalia Valle <tvallechavez4390@eagle.fgcu.edu>
2024-03-20 21:47:06 -0400

    Last queries and new rows for transaction table

--- Project Queries.sql
+++ Project Queries.sql
@@ -86,6 +86,85 @@ JOIN payment p ON t.payment = p.payment_id
 GROUP BY p.cash, p.credit, p.debit, i.category
 order by 4;
 
+/*
+
+#9 - Create a view to create rankings of times of the day (morning, afternoon, night) based on sales in order to have a better idea of when the store is busier. 
+#10 -  together with its category, price, etc. 
+
+*/
+
+-- #8 - Update reward points on the customer table based on tot_payment after a new row is inserted in the transaction table. 
+-- Creating Function 
+CREATE OR REPLACE FUNCTION update_reward_points()
+RETURNS TRIGGER AS $$
+DECLARE
+    total_payment NUMERIC;
+BEGIN
+    -- Get the total amount from the payment table
+    SELECT total_amount INTO total_payment
+    FROM payment
+    WHERE payment_id = NEW.payment;
+
+    -- $10 spent earns 1 reward point
+    UPDATE customer
+    SET reward_points = reward_points + total_payment / 10
+    WHERE customer_id = NEW.customer_id;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- -- Creating Trigger to update customer reward points
+CREATE TRIGGER trg_after_transaction_insert
+AFTER INSERT ON transaction
+FOR EACH ROW
+EXECUTE FUNCTION update_reward_points();
+
+
+-- #9  Create a view to create rankings of times of the day (morning, afternoon, evening)
+-- based on sales in order to have a better idea of when the store is bussier.
+CREATE OR REPLACE VIEW transactions_time_ranking AS
+SELECT
+    CASE
+        WHEN EXTRACT(HOUR FROM trans_date) < 12 THEN 'Morning'
+        WHEN EXTRACT(HOUR FROM trans_date) >= 12 AND EXTRACT(HOUR FROM trans_date) < 16 THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS day_times,
+    COUNT(*) AS transaction_count,
+    RANK() OVER (ORDER BY COUNT(*) DESC) as transaction_rank
+FROM transaction
+GROUP BY day_times
+ORDER BY transaction_rank;
+
+
+SELECT * FROM transactions_time_ranking
+
+
+-- #10 Create a function that takes a month and year as argument and returns the id of the item that sold the most during that month.
+CREATE OR REPLACE FUNCTION most_sold_item_id(month_input INT, year_input INT)
+RETURNS NUMERIC AS
+$$
+DECLARE
+    result_item_id NUMERIC;
+BEGIN
+    SELECT si.item_id INTO result_item_id
+    FROM sold_item si
+    JOIN transaction t ON si.trans_id = t.trans_id
+    WHERE EXTRACT(MONTH FROM t.trans_date) = month_input AND EXTRACT(YEAR FROM t.trans_date) = year_input
+    GROUP BY si.item_id
+    ORDER BY COUNT(si.item_id) DESC
+    LIMIT 1;
+
+    RETURN result_item_id;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+SELECT * FROM most_sold_item_id(8, 2023);
+
+
+
 
 	
 
